9bb3656cebe7919ad8d7ceb21dbf1992cb7ae5f0
phil <57152547+smartst3r@users.noreply.github.com>
2020-01-22 10:51:33 -0600

    Add files via upload

--- webcrawl.py
+++ webcrawl.py
@@ -3,7 +3,7 @@ import bs4
 from selenium import webdriver
 import time
 import pickle
-
+import xlsxwriter 
 ### scroll to bottom of page for how to search for players
 
 class Op_search():
@@ -17,6 +17,33 @@ class Op_search():
 	
 	def __str__(self):
 		return str(self.info)
+		
+	def save_xlsx(self):
+		#saves a excel sheet !not formated! 
+		wb = xlsxwriter.Workbook('Saved-excels/'+''.join(self.username)+'.xlsx')
+		ws = wb.add_worksheet() 
+		count_username=0
+		column=0
+		for u in self.username:
+			count_season=0
+			row=0
+			ws.write(row,column,u)
+			for s in self.season:
+				row+=1
+				ws.write(row,column,s)
+				for champion in self.info[count_username].get(u)[count_season]:
+					row+=1
+					name=champion.get('Name')
+					winRate=champion.get('winRate')
+					gamesplayed=champion.get('gamesWon')+champion.get('gamesLost')
+					format_stats=name+' '+str(int(float(winRate[:-1])))+'% '+str(gamesplayed)
+					ws.write(row,column,format_stats)
+				row=0
+				column+=1
+				count_season+=1
+			count_username+=1	
+		wb.close() 
+				
 		
 	def save(self):
 		count_username=0
@@ -41,19 +68,20 @@ class Op_search():
 					writer.write(u+'\n'+' \n')
 					for i in objects[0]:
 						writer.write(i.get('Name')+' ')
-						writer.write(i.get('Win rate'+'winRate')+' ')
+						writer.write('Win rate'+i.get('winRate')+' ')
 						writer.write('Games played: '+str(i.get('gamesWon')+i.get('gamesLost'))+' ')
 						writer.write(' \n'+' \n')
 			count_username+=1
 		
 	def opSearch(self,username,season):
-		#starts loop with first username then first season then keeps appending info to master list data format returned list[dictionary:list]
+		#starts loop with first username then first season then keeps appending info to master list data format returned #list[{username:list[{name:,winRate:,gamesWon:,gamesLost:}]}]
 		master_list=[]
 		driver = webdriver.Chrome()
 		for u in self.username:
 			url =self.base_url+u
 			driver.get(url)
 			temp=[]
+			driver.execute_script("window.scrollTo(0, 1000)") 
 			for s in self.season:
 				#loading page
 				driver.find_element_by_id('left_champion').click()
@@ -99,5 +127,6 @@ class Op_search():
 #season 10 = 'season-15' ||| season 9 = 'season-13'
 #please input list or you will get errors
 ###UNCOMMENT EXAMPLE BELOW TO SEE HOW THIS WORKS 
-#search=Op_search(['Smartster','SgtNutBuster'],['season-13','season-15'])
+# search=Op_search(['Smartster','SgtNutBuster'],['season-13','season-15'])
 # search.save()
+# search.save_xlsx()
